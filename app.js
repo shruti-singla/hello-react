0d6c8085fdae1e9fd0743a50d7402ffe64ae4d5b
shruti <99singla.shruti.gmail.com>
2023-06-13 22:53:29 +0530

    ep-3

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-//const jsxHeading = <h1>hello react using jsx</h1>;
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
+const jsxHeading = <h1>hello react using jsx</h1>;
 // functional component
-
 
 const Title = () => <h1>hello react using arrow function</h1>;
 
+const data = 100;
 
 const HeadingComponent = () => (
-    <div id = "container">
-        <Title />
-        <h1>hello from frunctional component</h1>
-    </div>
+  <div id="container">
+    <Title />
+    {data}
+    <h1>hello from functional component</h1>
+    {DemoComponent()}
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
+
+const DemoComponent = () => (
+  <>
+    <div id="container1">
+      <h1>hello from demo1</h1>
+    </div>
+    <div id="container2">
+      <h1>hello from demo2</h1>
+    </div>
+    <>
+      <div id="container3">
+        <h1>hello from demo1</h1>
+      </div>
+      <div id="container4">
+        <h1>hello from demo2</h1>
+      </div>
+    </>
+  </>
+);
